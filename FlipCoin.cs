19cb5e6b8cfa3e1127a48b80e20d0aa69ead305a
Ambili das v s <ambili.kan@gmail.com>
2022-05-06 16:57:19 +0530

    Program for coin flip is added

---
+++ FlipCoin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoinProgram
+{
+    internal class FlipCoin
+    {
+        public void headsAndTails()
+        {
+            Console.Write("Enter the number of flips ");
+           double num_of_flips = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+          double Heads = 0; double Tails = 0;
+
+            if (num_of_flips > 0)
+            {
+                for (int i = 1; i <= num_of_flips; i++)
+                {
+
+                    int coin = random.Next(0, 2);
+                    if (coin == 0)
+                    {
+                        Console.WriteLine("HEADS");
+                        Heads++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("TAILS");
+                        Tails++;
+                    }
+                }
+                
+                double percentage_head = Math.Round((Heads / num_of_flips)*100) ;
+                double percentage_tail = Math.Round((Tails / num_of_flips)*100) ;
+                Console.WriteLine("Percentage of Heads: " + percentage_head +"%");
+                Console.WriteLine("Percentage of Tails: " + percentage_tail +"%");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FlipCoinProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FLIP COIN AND PRINT PERCENTAGE OF HEADS AND TAILS");
+            FlipCoin myobj=new FlipCoin();
+            myobj.headsAndTails();
+        }
+    }
+}
